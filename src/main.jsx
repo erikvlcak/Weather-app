cb60cf809185e0b799235d280d7aad652c5f2b56
erikvlcak <erikvlcak1@gmail.com>
2024-09-15 18:24:10 +0200

    adjust app path

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-
-import App from './app'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
