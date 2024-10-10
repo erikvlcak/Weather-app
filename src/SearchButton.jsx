6bcc02adf400048ae7a9d2ab7cae92ec468423e3
erikvlcak <erikvlcak1@gmail.com>
2024-10-11 00:54:22 +0200

    adjust width of search button on mobile

--- src/SearchButton.jsx
+++ src/SearchButton.jsx
@@ -20,7 +20,7 @@ export default function SearchButton({
       }}
       type="button"
       //className="rounded-xl bg-[#FFAFCC] border-4 border-[#FFAFCC] text-white hover:bg-[#FFC8DD] text-2xl font-bold px-10 py-3 shadow-lg hover:shadow-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-all"
-      className=" h-full rounded-xl border-4 text-2xl px-10 py-3 font-bold relative flex w-40 items-center justify-center overflow-hidden bg-[#FFC8DD] text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-[#FFAFCC] before:duration-500 before:ease-out hover:shadow-[#FFAFCC] hover:before:h-56 hover:before:w-56"
+      className="select-none h-full w-full rounded-xl border-4 text-2xl px-10 py-3 font-bold relative flex items-center justify-center overflow-hidden bg-[#FFC8DD] text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-[#FFAFCC] before:duration-500 before:ease-out hover:shadow-[#FFAFCC] hover:before:h-56 hover:before:w-56"
     >
       <span className="relative z-10">Search</span>
     </button>
